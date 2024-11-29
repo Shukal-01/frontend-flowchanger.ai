39d5b8729d309466b60a7e5df5452aac03a59249
piyush <piyushloona@gmail.com>
2024-11-29 10:35:17 +0530

    update delete branch modal ui and add csv function branchdetials page

--- src/pages/Admin_Panel/Branch/AddBranch.jsx
+++ src/pages/Admin_Panel/Branch/AddBranch.jsx
@@ -3,23 +3,28 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useGlobalContext } from '../../../Context/GlobalContext'
 
 const AddBranch = () => {
-    const navigate=useNavigate();
-    const {baseUrl,openToast}=useGlobalContext();
-    const [branchName, setBranchName]=React.useState("");
-    async function addBranch(){
-        const result=await fetch(baseUrl+"branch", {
-            method:"POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({branchName:branchName})
-        });
-        if(result.status==201){
-            openToast("Branch Added Successfully","success");
-            navigate("/branch-details");
+    const navigate = useNavigate();
+    const { baseUrl, openToast } = useGlobalContext();
+    const [branchName, setBranchName] = React.useState("");
+    async function addBranch() {
+        try {
+            const result = await fetch(baseUrl + "branch", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ branchName: branchName })
+            });
+            if (result.status == 201) {
+                openToast("Branch Added Successfully", "success");
+                navigate("/branch-details");
+            }
+            else {
+                openToast("Something went wrong", "error");
+            }
         }
-        else{
-            openToast("Something went wrong","error");
+        catch (error) {
+            console.log("error", error)
         }
     }
 
@@ -28,10 +33,10 @@ const AddBranch = () => {
             <h2 className='text-[#27004a] font-medium text-[18px]'>Add New Branch</h2>
 
             <div className='w-[100%]'>
-            <div className='bg-[#fff] w-[100%] md:w-[80%] xl:w-[50%] lg:w-[50%] set-shadow p-3 mt-2 rounded-md'>
-            <label>Branch Name</label><br />
+                <div className='bg-[#fff] w-[100%] md:w-[80%] xl:w-[50%] lg:w-[50%] set-shadow p-3 mt-2 rounded-md'>
+                    <label>Branch Name</label><br />
                     <input type='text' className='mt-2 border border-1 pl-3 h-[43px] pr-7
-            ] rounded-md focus:outline-none w-[100%] text-[15px] text-[#aeabab]' onChange={(e)=>setBranchName(e.target.value)}/>
+            ] rounded-md focus:outline-none w-[100%] text-[15px] text-[#aeabab]' onChange={(e) => setBranchName(e.target.value)} />
                     <div className='text-end mt-3 flex gap-[10px] justify-end'>
                         <Link to="/branch-details" className='first-btn flex items-center pt-2 py-2 pl-5 pr-5 rounded-md text-white hover:bg-[#7526d1]'>
                             Cancel
